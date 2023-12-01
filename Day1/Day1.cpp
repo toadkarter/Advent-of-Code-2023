af146e3572737c5d3505521c87c305d3bfaead1f
toadkarter <vrakhmanin1@gmail.com>
2023-12-01 08:24:30 +0000

    initial version of solution

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <fstream>
+
+int get_double_digit(const std::string& line) {
+  char first_digit = '\0';
+  char second_digit = '\0';
+
+  for (const char current_char : line) {
+    if (std::isdigit(current_char)) {
+      if (first_digit == '\0') {
+        first_digit = current_char;
+      } else {
+        second_digit = current_char;
+      }
+    }
+  }
+
+  return (first_digit - '0') * 10 + (second_digit - '0');
+}
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    std::ifstream input_file("input.txt");
     
+    int final_sum = 0;
+    std::string current_line;
+    while (getline(input_file, current_line)) {
+      final_sum += get_double_digit(current_line);
+    }
+
+    std::cout << final_sum << std::endl;
+
+    return 0;
 }
